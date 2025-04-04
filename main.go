688b8f21ccab6283c2794adc8dc8b1c7578a53de
Priyansh Rai <priyanshrai16@gmail.com>
2025-04-04 16:06:38 +0530

    Add JSON response handler to HTTP server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -13,9 +14,16 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
+func handleJSON(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"message": "Hello, JSON!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func createServer() error {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /", handleRoot)
+	mux.HandleFunc("GET /json", handleJSON)
 
 	fmt.Println("Starting server on http://localhost" + PORT)
 
